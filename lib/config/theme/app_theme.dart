b59d519ee0bd4b3850981a870f4e3602bd8d0a91
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-07 09:41:25 -0600

    MenuItems class created

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -13,6 +13,8 @@ class AppTheme {
   AppTheme({this.selectedColor = 0})
       : assert((selectedColor < 0 || selectedColor > 3), 'Mal color mal');
 
-  ThemeData getTheme() =>
-      ThemeData(useMaterial3: true, colorSchemeSeed: colors[selectedColor]);
+  ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colors[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: true));
 }
