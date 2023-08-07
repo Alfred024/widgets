96229389850e3756a1b4603b590c2ea234e0d5ad
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-07 09:22:10 -0600

    First commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const colors = <Color>[
+  Colors.lightBlue,
+  Colors.indigo,
+  Colors.deepPurple,
+  Colors.purple,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert((selectedColor < 0 || selectedColor > 3), 'Mal color mal');
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colors[selectedColor]);
+}
