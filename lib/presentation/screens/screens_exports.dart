4c92c01d743d9dddc24852b9849442b8a149d25f
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-07 21:13:55 -0600

    Created templates of screen widgets

--- lib/presentation/screens/screens_exports.dart
+++ lib/presentation/screens/screens_exports.dart
@@ -1,3 +1,8 @@
+import 'package:widgets/presentation/screens/animated/animated_screens.dart';
+import 'package:widgets/presentation/screens/infinite_scroll/infinite_scroll.dart';
+import 'package:widgets/presentation/screens/progress/progress.dart';
+import 'package:widgets/presentation/screens/snackbar/snackbar.dart';
+import 'package:widgets/presentation/screens/ui_controls/ui_controls.dart';
 export 'package:widgets/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets/presentation/screens/home/home_screen.dart';
 export 'package:widgets/presentation/screens/buttons/buttons_screen.dart';
