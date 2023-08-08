d0b6144015eb32a8ce010d1342ca97b64227fa88
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-07 23:20:11 -0600

    implemeted buttons that displays dialogs of Flutter

--- lib/presentation/screens/snackbar/snackbar.dart
+++ lib/presentation/screens/snackbar/snackbar.dart
@@ -15,11 +15,46 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(customSnackBar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Alert dialog'),
+              content: const Text(
+                  'Este es un método de flutter que despliega un Widget que notifica algo. '),
+              actions: [
+                TextButton(onPressed: () {}, child: const Text('Salir')),
+                FilledButton(onPressed: () {}, child: const Text('Aceptar'))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(
+                      context: context,
+                      applicationName: 'Show about dialog',
+                      children: [
+                        const Text(
+                            'Displays an [AboutDialog], which describes the application and provides a button to show licenses for software used by the application.')
+                      ]);
+                },
+                child: const Text('Show licenses')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Alert dialog')),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Show snackbar'),
