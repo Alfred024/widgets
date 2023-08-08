247d41da2f05b0e1d217f36dcd6a888ac3992d90
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-07 20:30:36 -0600

    Cards V1 created in Cardsscreen

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
   const CardsScreen({super.key});
@@ -15,6 +24,65 @@ class CardsScreen extends StatelessWidget {
               context.pop();
             },
             icon: const Icon(Icons.arrow_back_ios)),
+      ),
+      body: const _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        ...cards.map((card) => _CardType1(
+              label: card['label'],
+              elevation: card['elevation'],
+            )),
+        ...cards.map((card) => _CardType1(
+              label: card['label'],
+              elevation: card['elevation'],
+            ))
+      ],
+    ));
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            //Align(),
+            SizedBox(
+              //width: double.maxFinite,
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_vert_outlined,
+                        color: Colors.deepPurpleAccent,
+                      ))),
+            ),
+            SizedBox(
+              //width: double.maxFinite,
+              child: Align(alignment: Alignment.bottomLeft, child: Text(label)),
+            ),
+          ],
+        ),
       ),
     );
   }
