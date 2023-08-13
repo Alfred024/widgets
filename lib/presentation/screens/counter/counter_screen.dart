cadee5a2f39afb4c009eef49502a8b0aef0738ce
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-13 14:02:24 -0600

    Using new provider to dynamicly change IconTheme

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets/presentation/providers/counter_provider.dart';
+import 'package:widgets/presentation/providers/theme_provider.dart';
 
 // ignore: must_be_immutable
 class CounterScreen extends ConsumerWidget {
@@ -12,16 +13,30 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int counterValue = ref.watch(counterProvider);
+    final bool darkMode = ref.watch(darkModeOn);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(darkModeOn.notifier).update((state) => !state);
+              },
+              icon: darkMode
+                  ? const Icon(Icons.light_mode)
+                  : const Icon(Icons.dark_mode))
+        ],
       ),
       body: Center(
         child: Text('Contador: $counterValue'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+          //ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
